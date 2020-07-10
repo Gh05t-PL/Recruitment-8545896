6d8e0e2f9f3f80cb15d374909618f3f2307e1e18
gh05t-pl <gozetero@gmail.com>
2020-07-10 12:30:22 +0200

    Added NumberScale validator
    for validating scale of set number

---
+++ src/Validator/NumberScaleValidator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class NumberScaleValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        /* @var $constraint NumberScale */
+
+        if ( null === $value || '' === $value ) {
+            return;
+        }
+
+        if ( $this->getDecCount($constraint->scale) ) {
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ value }}', $value)
+                ->addViolation();
+        }
+    }
+
+    private function getDecCount($f)
+    {
+        $num = 0;
+        while ( true ) {
+            if ( (string)$f === (string)round($f) ) {
+                break;
+            }
+            if ( is_infinite($f) ) {
+                break;
+            }
+
+            $f *= 10;
+            $num++;
+        }
+        return $num;
+    }
+}
