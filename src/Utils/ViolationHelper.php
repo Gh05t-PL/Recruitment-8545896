8b59bccbf09be1d3371cce2774d499a4f5d7781f
gh05t-pl <gozetero@gmail.com>
2020-07-10 12:32:37 +0200

    added violation helper
    for normalization of violations list which next will be provided in responses

---
+++ src/Utils/ViolationHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Utils;
+
+
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+
+class ViolationHelper
+{
+    static public function normalizeViolations(ConstraintViolationListInterface $violations)
+    {
+        $formatedViolationList = [];
+        for ( $i = 0; $i < $violations->count(); $i++ ) {
+            $violation = $violations->get($i);
+            $formatedViolationList[] = [
+                'property' => $violation->getPropertyPath(),
+                'message' => $violation->getMessage(),
+                'code' => $violation->getCode()
+            ];
+        }
+        return $formatedViolationList;
+    }
+}
