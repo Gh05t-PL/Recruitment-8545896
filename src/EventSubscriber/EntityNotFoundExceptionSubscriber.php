0f9f3575e93a81abcc0d2daa44b53b987aeddd29
gh05t-pl <gozetero@gmail.com>
2020-07-10 13:26:15 +0200

    added EntityNotFound Exception and it's subscriber

---
+++ src/EventSubscriber/EntityNotFoundExceptionSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Exceptions\EntityNotFoundException;
+use App\Exceptions\ValidationException;
+use App\Utils\ApiHelper;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+class EntityNotFoundExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        if ( $exception instanceof EntityNotFoundException ) {
+            $event->setResponse(new JsonResponse(
+                ApiHelper::prepareResponse(
+                    false,
+                    null
+                ),
+                Response::HTTP_NOT_FOUND
+            ));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.exception' => 'onKernelException',
+        ];
+    }
+}
